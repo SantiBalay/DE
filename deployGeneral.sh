203c9a98c957dce8d5fca3f57e5237bd5fcd7528
SantiBalay <santiagobalay1997@hotmail.com>
2019-11-01 19:27:23 -0300

    Agrego deploy generico

---
+++ deployGeneral.sh
@@ -0,0 +1,23 @@
+echo "Iniciando deploy..."
+
+cd ~/
+
+echo " /-----------------------------------------------------------------------------------------/"
+echo " /-----------------------------------------------------------------------------------------/"
+echo "Clonando commons"
+git clone https://github.com/sisoputnfrba/so-commons-library.git
+cd ~/so-commons-library
+echo "Instalando commons"
+make
+
+echo " /-----------------------------------------------------------------------------------------/"
+echo " /-----------------------------------------------------------------------------------------/"
+echo "Clonando proyecto"
+cd ~
+git clone https://github.com/sisoputnfrba/tp-2019-2c-NoCurseCreatividad.git
+
+echo " /-----------------------------------------------------------------------------------------/"
+echo " /-----------------------------------------------------------------------------------------/"
+echo "Compilando librerias estaticas"
+cd ~/tp-2019-2c-NoCurseCreatividad/herramientas/Debug/
+make
