3e8a0d23778bc294e0b7d7061105a60da7cea9c6
manumaso <manuelmaso@hotmail.com>
2019-11-02 00:18:08 -0300

    arreglo script bash de hilolay

--- deploy-SUSE.sh
+++ deploy-SUSE.sh
@@ -1,23 +1,24 @@
-cd ~/
-
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
-echo "Compilando librerias estaticas"
-cd ~/tp-2019-2c-NoCurseCreatividad/herramientas/Debug/
-make
-
-
-
-echo " /-----------------------------------------------------------------------------------------/"
-echo " /-----------------------------------------------------------------------------------------/"
-echo "Compilando Hilolay"
+echo "Compilando HilolayNuestro"
 cd ~/tp-2019-2c-NoCurseCreatividad/HilolayNuestro/Debug
 make
 
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
-echo "Agregando Hilolay como libreria externa"
+echo "Agregando HilolayNuestro como libreria externa"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/HilolayNuestro/Debug
+
+echo " /-----------------------------------------------------------------------------------------/"
+echo " /-----------------------------------------------------------------------------------------/"
+echo "Compilando Hilolay"
+cd ~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug
+make
+
+echo " /-----------------------------------------------------------------------------------------/"
+echo " /-----------------------------------------------------------------------------------------/"
+echo "Agregando Hilolay como libreria externa"
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug
 
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
