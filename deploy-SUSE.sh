fda50a4ec26e5939b2feec4130f0383617a8d6e0
manumaso <manuelmaso@hotmail.com>
2019-12-20 10:41:34 -0300

    modifico deploy suse

--- deploy-SUSE.sh
+++ deploy-SUSE.sh
@@ -1,14 +1,22 @@
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
+echo "Instalando Hilolay"
+cd ~
+git clone https://github.com/sisoputnfrba/hilolay.git
+cd hilolay
+cp ~/tp-2019-2c-NoCurseCreatividad/Hilolay/* ~/hilolay
+
 echo "Compilando Hilolay"
-cd ~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug
 make
+make install
 
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
 echo "Agregando Hilolay como libreria externa"
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug
-
+cd ~
+echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug" >> .bashrc
+source .bashrc
+ 
 echo " /-----------------------------------------------------------------------------------------/"
 echo " /-----------------------------------------------------------------------------------------/"
 echo "Imprimiendo LD_LIBRARY_PATH..."
