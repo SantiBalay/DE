c8d9e9b0178275f33a5d5ee8c05a48dcbf9cc795
CimimUxMaio <cimimuxmaio2@gmail.com>
2019-12-21 01:45:02 -0300

    Ahora se compilan todos los tests al final de 'deploy-TESTS.sh'

--- deploy-TESTS.sh
+++ deploy-TESTS.sh
@@ -1,27 +1,33 @@
-echo " /-----------------------------------------------------------------------------------------/"
-echo " /-----------------------------------------------------------------------------------------/"
-
+echo " "
+echo " "
 echo "Compilando Hilolay"
 cd ~/tp-2019-2c-NoCurseCreatividad/Hilolay/
 sudo make
 
-echo " /-----------------------------------------------------------------------------------------/"
-echo " /-----------------------------------------------------------------------------------------/"
+echo " "
+echo " "
 echo "Agregando Hilolay como libreria externa"
 cd ~
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/Hilolay/Debug" >> .bashrc
 source .bashrc
 
 
-echo " /-----------------------------------------------------------------------------------------/"
-echo " /-----------------------------------------------------------------------------------------/"
+echo " "
+echo " "
 echo "Compilando libmuse"
 cd ~/tp-2019-2c-NoCurseCreatividad/muse/
 sudo make
 
-echo " /-----------------------------------------------------------------------------------------/"
-echo " /-----------------------------------------------------------------------------------------/"
+echo " "
+echo " "
 echo "Agregando libmuse como libreria externa"
 cd ~
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/tp-2019-2c-NoCurseCreatividad/muse/Debug" >> .bashrc
 source .bashrc
+
+
+echo " "
+echo " "
+echo "Compilando todos los tests"
+cd ~/tp-2019-2c-NoCurseCreatividad/TESTS/
+make all
